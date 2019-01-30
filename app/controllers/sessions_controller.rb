375025d1295eae4eeea576c493e9978d6446b1e3
Gilbert Torchon <gilbert@noukod.com>
2019-01-30 20:56:31 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     puts params.inspect
     @user=User.find_by(name:params[:user][:name])
-    
+
 
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id]=@user.id
